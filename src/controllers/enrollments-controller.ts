ea405cb97af8eeab73e5926b1d5f6c835388eb6d
Hugo Junior <hjunioreng@gmail.com>
2023-04-08 10:49:22 -0300

    feat: enrollments

--- src/controllers/enrollments-controller.ts
+++ src/controllers/enrollments-controller.ts
@@ -2,12 +2,15 @@ import { Response } from 'express';
 import httpStatus from 'http-status';
 import { AuthenticatedRequest } from '@/middlewares';
 import enrollmentsService from '@/services/enrollments-service';
+//import { string } from 'joi';
 
 export async function getEnrollmentByUser(req: AuthenticatedRequest, res: Response) {
   const { userId } = req;
 
   try {
     const enrollmentWithAddress = await enrollmentsService.getOneWithAddressByUserId(userId);
+
+    //    console.log(enrollmentWithAddress, "teste 2")
 
     return res.status(httpStatus.OK).send(enrollmentWithAddress);
   } catch (error) {
@@ -29,12 +32,16 @@ export async function postCreateOrUpdateEnrollment(req: AuthenticatedRequest, re
 }
 
 export async function getAddressFromCEP(req: AuthenticatedRequest, res: Response) {
+  const { dataCep } = req.body;
+
   try {
-    const address = await enrollmentsService.getAddressFromCEP();
-    res.status(httpStatus.OK).send(address);
+    const address = await enrollmentsService.getAddressFromCEP(dataCep);
+
+    //   console.log(address,"testeeeee")
+    return res.status(httpStatus.OK).send(address);
   } catch (error) {
     if (error.name === 'NotFoundError') {
-      return res.send(httpStatus.NO_CONTENT);
+      return res.sendStatus(httpStatus.NO_CONTENT);
     }
   }
 }
